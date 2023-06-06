239003b140516d86f09a1144233b292f0c0f45e4
陈柱锋 <2414802324@qq.com>
2023-06-06 10:51:39 +0800

    bugfix：评论相关接口

--- documentservice/controllers/comment.go
+++ documentservice/controllers/comment.go
@@ -25,7 +25,7 @@ const (
 )
 
 type UserComment struct {
-	Id              string            `json:"id" bson:"id"`
+	Id              string            `json:"id" bson:"_id"`
 	ParentId        string            `json:"parent_id" bson:"parent_id"`
 	RootId          string            `json:"root_id" bson:"root_id"`
 	DocumentId      string            `json:"doc_id" bson:"document_id" binding:"required"`
@@ -34,8 +34,8 @@ type UserComment struct {
 	TargetShapeId   string            `json:"target_shape_id" bson:"target_shape_id" binding:"required"`
 	ShapeFrame      map[string]any    `json:"shape_frame" bson:"shape_frame"`
 	UserId          string            `json:"user_id" bson:"user_id"`
-	CreatedAt       myTime.Time       `json:"created_at" bson:"created_at"`
-	RecordCreatedAt myTime.Time       `json:"record_created_at" bson:"record_created_at"`
+	CreatedAt       string            `json:"created_at" bson:"created_at"`
+	RecordCreatedAt string            `json:"record_created_at" bson:"record_created_at"`
 	Content         string            `json:"content" bson:"content" binding:"required"`
 	Status          UserCommentStatus `json:"status" bson:"status"`
 }
@@ -107,8 +107,8 @@ func PostUserComment(c *gin.Context) {
 	}
 	userComment.Id = str.IntToString(snowflake.NextId())
 	userComment.UserId = str.IntToString(userId)
-	userComment.CreatedAt = myTime.Time(time.Now())
-	if userComment.RecordCreatedAt.IsZero() {
+	userComment.CreatedAt = myTime.Time(time.Now()).String()
+	if _, err := myTime.Parse(userComment.CreatedAt); err != nil {
 		userComment.RecordCreatedAt = userComment.CreatedAt
 	}
 	userComment.Status = UserCommentStatusCreated
@@ -124,7 +124,7 @@ var errNoPermission = errors.New("无权限")
 
 func checkUserPermission(userId int64, commentId string) (*UserComment, error) {
 	commentCollection := mongo.DB.Collection("comment")
-	commentRes := commentCollection.FindOne(nil, bson.M{"id": commentId})
+	commentRes := commentCollection.FindOne(nil, bson.M{"_id": commentId})
 	if commentRes.Err() != nil {
 		return nil, errors.New("评论不存在")
 	}
@@ -141,7 +141,7 @@ func checkUserPermission(userId int64, commentId string) (*UserComment, error) {
 }
 
 type UserCommentUpdate struct {
-	Id            string         `json:"id" bson:"id"`
+	Id            string         `json:"id" bson:"_id"`
 	ParentId      string         `json:"parent_id" bson:"parent_id"`
 	RootId        string         `json:"root_id" bson:"root_id"`
 	PageId        string         `json:"page_id" bson:"page_id,omitempty"`
@@ -181,7 +181,7 @@ func PutUserComment(c *gin.Context) {
 		return
 	}
 	commentCollection := mongo.DB.Collection("comment")
-	if _, err := commentCollection.UpdateOne(nil, bson.M{"id": userComment.Id}, bson.M{"$set": &userComment}); err != nil {
+	if _, err := commentCollection.UpdateByID(nil, userComment.Id, bson.M{"$set": &userComment}); err != nil {
 		log.Println("mongo更新失败", err)
 		response.Fail(c, "更新失败")
 	}
@@ -215,7 +215,7 @@ func DeleteUserComment(c *gin.Context) {
 			response.Forbidden(c, "")
 			return
 		}
-		commentRes := commentCollection.FindOne(nil, bson.M{"id": comment.ParentId})
+		commentRes := commentCollection.FindOne(nil, bson.M{"_id": comment.ParentId})
 		if commentRes.Err() == nil {
 			response.Forbidden(c, "")
 			return
@@ -231,7 +231,7 @@ func DeleteUserComment(c *gin.Context) {
 			return
 		}
 	}
-	if _, err := commentCollection.DeleteOne(nil, bson.M{"id": commentId}); err != nil {
+	if _, err := commentCollection.DeleteOne(nil, bson.M{"_id": commentId}); err != nil {
 		log.Println("mongo删除失败", err)
 		response.Fail(c, "删除失败")
 	}
@@ -239,7 +239,7 @@ func DeleteUserComment(c *gin.Context) {
 }
 
 type UserCommentSetStatus struct {
-	Id     string            `json:"id" bson:"id"`
+	Id     string            `json:"id" bson:"_id"`
 	Status UserCommentStatus `json:"status" bson:"status"`
 }
 
@@ -284,7 +284,7 @@ func SetUserCommentStatus(c *gin.Context) {
 		}
 	}
 	commentCollection := mongo.DB.Collection("comment")
-	if _, err := commentCollection.UpdateOne(nil, bson.M{"id": userComment.Id}, bson.M{"$set": &userComment}); err != nil {
+	if _, err := commentCollection.UpdateByID(nil, userComment.Id, bson.M{"$set": &userComment}); err != nil {
 		log.Println("mongo更新失败", err)
 		response.Fail(c, "更新失败")
 	}
