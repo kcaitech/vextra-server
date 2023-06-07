665da33d12ae857b2590ef7e22e530bb8fc9e6cb
陈柱锋 <2414802324@qq.com>
2023-06-07 10:56:07 +0800

    微信登陆接口支持使用旧code、用户id、邀请码进行登录

--- authservice/controllers/wx_login.go
+++ authservice/controllers/wx_login.go
@@ -85,154 +85,140 @@ func WxLogin(c *gin.Context) {
 	}
 
 	userService := services.NewUserService()
+	user := &models.User{}
 
 	if req.Id != "" {
-		user := &models.User{}
 		if err := userService.Get(user, "id = ? and wx_login_code = ?", req.Id, req.Code); err != nil {
 			response.BadRequest(c, "参数错误：id")
 			return
 		}
-		// 创建JWT
-		token, err := jwt.CreateJwt(&jwt.Data{
-			Id:       str.IntToString(user.Id),
-			Nickname: user.Nickname,
-		})
-		if err != nil {
-			response.Fail(c, err.Error())
-			return
-		}
-		response.Success(c, &wxLoginResp{
-			Id:       str.IntToString(user.Id),
-			Nickname: user.Nickname,
-			Token:    token,
-			Avatar:   user.Avatar,
-		})
-		return
-	}
-
-	// Code换取AccessToken
-	// 发起请求
-	queryParams := url.Values{}
-	queryParams.Set("appid", config.Config.Wx.Appid)
-	queryParams.Set("secret", config.Config.Wx.Secret)
-	queryParams.Set("code", req.Code)
-	queryParams.Set("grant_type", "authorization_code")
-	resp, err := http.Get("https://api.weixin.qq.com/sns/oauth2/access_token" + "?" + queryParams.Encode())
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	defer resp.Body.Close()
-	// 读取响应
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	// json解析
-	var wxAccessTokenResp wxAccessTokenResp
-	err = json.Unmarshal(body, &wxAccessTokenResp)
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	if wxAccessTokenResp.Openid == "" {
-		log.Println("OpenId为空")
-		response.Fail(c, "登陆失败")
-		return
-	}
-
-	// AccessToken换取用户信息
-	// 发起请求
-	queryParams = url.Values{}
-	queryParams.Set("access_token", wxAccessTokenResp.AccessToken)
-	queryParams.Set("openid", wxAccessTokenResp.Openid)
-	queryParams.Set("lang", "lang")
-	resp, err = http.Get("https://api.weixin.qq.com/sns/userinfo" + "?" + queryParams.Encode())
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	defer resp.Body.Close()
-	// 读取响应
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	// json解析
-	var wxUserInfoResp wxUserInfoResp
-	err = json.Unmarshal(body, &wxUserInfoResp)
-	if err != nil {
-		log.Println(err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	if wxUserInfoResp.Nickname == "" || wxUserInfoResp.Headimgurl == "" {
-		log.Println("Nickname或Headimgurl为空")
-		response.Fail(c, "登陆失败")
-		return
-	}
-
-	user := &models.User{}
-	err = userService.Get(user, "wx_open_id = ?", wxAccessTokenResp.Openid)
-	if err != nil && err != services.ErrRecordNotFound {
-		log.Println("userService.Get错误：", err)
-		response.Fail(c, "登陆失败")
-		return
-	}
-	if err == services.ErrRecordNotFound {
-		// 创建用户
-		t := Time(time.Now())
-		user = &models.User{
-			Nickname:                 wxUserInfoResp.Nickname,
-			WxOpenId:                 wxAccessTokenResp.Openid,
-			WxAccessToken:            wxAccessTokenResp.AccessToken,
-			WxAccessTokenCreateTime:  t,
-			WxRefreshToken:           wxAccessTokenResp.RefreshToken,
-			WxRefreshTokenCreateTime: t,
-			Avatar:                   wxUserInfoResp.Headimgurl,
-			Uid:                      str.GetUid(),
-			WxLoginCode:              req.Code,
-		}
-		err := userService.Create(user)
+	} else {
+		// Code换取AccessToken
+		// 发起请求
+		queryParams := url.Values{}
+		queryParams.Set("appid", config.Config.Wx.Appid)
+		queryParams.Set("secret", config.Config.Wx.Secret)
+		queryParams.Set("code", req.Code)
+		queryParams.Set("grant_type", "authorization_code")
+		resp, err := http.Get("https://api.weixin.qq.com/sns/oauth2/access_token" + "?" + queryParams.Encode())
 		if err != nil {
 			log.Println(err)
 			response.Fail(c, "登陆失败")
 			return
 		}
-		// 下载微信头像
-		go func(user *models.User, avatarUrl string) {
-			resp, err := http.Get(avatarUrl)
+		defer resp.Body.Close()
+		// 读取响应
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		// json解析
+		var wxAccessTokenResp wxAccessTokenResp
+		err = json.Unmarshal(body, &wxAccessTokenResp)
+		if err != nil {
+			log.Println(err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		if wxAccessTokenResp.Openid == "" {
+			log.Println("OpenId为空")
+			response.Fail(c, "登陆失败")
+			return
+		}
+
+		// AccessToken换取用户信息
+		// 发起请求
+		queryParams = url.Values{}
+		queryParams.Set("access_token", wxAccessTokenResp.AccessToken)
+		queryParams.Set("openid", wxAccessTokenResp.Openid)
+		queryParams.Set("lang", "lang")
+		resp, err = http.Get("https://api.weixin.qq.com/sns/userinfo" + "?" + queryParams.Encode())
+		if err != nil {
+			log.Println(err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		defer resp.Body.Close()
+		// 读取响应
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		// json解析
+		var wxUserInfoResp wxUserInfoResp
+		err = json.Unmarshal(body, &wxUserInfoResp)
+		if err != nil {
+			log.Println(err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		if wxUserInfoResp.Nickname == "" || wxUserInfoResp.Headimgurl == "" {
+			log.Println("Nickname或Headimgurl为空")
+			response.Fail(c, "登陆失败")
+			return
+		}
+
+		err = userService.Get(user, "wx_open_id = ?", wxAccessTokenResp.Openid)
+		if err != nil && err != services.ErrRecordNotFound {
+			log.Println("userService.Get错误：", err)
+			response.Fail(c, "登陆失败")
+			return
+		}
+		if err == services.ErrRecordNotFound {
+			// 创建用户
+			t := Time(time.Now())
+			user = &models.User{
+				Nickname:                 wxUserInfoResp.Nickname,
+				WxOpenId:                 wxAccessTokenResp.Openid,
+				WxAccessToken:            wxAccessTokenResp.AccessToken,
+				WxAccessTokenCreateTime:  t,
+				WxRefreshToken:           wxAccessTokenResp.RefreshToken,
+				WxRefreshTokenCreateTime: t,
+				Avatar:                   wxUserInfoResp.Headimgurl,
+				Uid:                      str.GetUid(),
+				WxLoginCode:              req.Code,
+			}
+			err := userService.Create(user)
 			if err != nil {
-				log.Println("下载头像失败：", err)
+				log.Println(err)
+				response.Fail(c, "登陆失败")
 				return
 			}
-			defer resp.Body.Close()
-			if _, err := services.NewUserService().UploadAvatar(user, resp.Body, resp.ContentLength, resp.Header.Get("Content-Type")); err != nil {
-				log.Println("上传头像失败：", err)
-				return
-			}
-		}(user, wxUserInfoResp.Headimgurl)
+			// 下载微信头像
+			go func(user *models.User, avatarUrl string) {
+				resp, err := http.Get(avatarUrl)
+				if err != nil {
+					log.Println("下载头像失败：", err)
+					return
+				}
+				defer resp.Body.Close()
+				if _, err := services.NewUserService().UploadAvatar(user, resp.Body, resp.ContentLength, resp.Header.Get("Content-Type")); err != nil {
+					log.Println("上传头像失败：", err)
+					return
+				}
+			}(user, wxUserInfoResp.Headimgurl)
+		}
 	}
+
 	if !user.IsActivated {
+		user.WxLoginCode = req.Code
+		_ = userService.Updates(user)
 		// 邀请码校验
 		if len(sliceutil.FilterT(func(code string) bool {
 			return req.InviteCode == code
 		}, InviteCodeList...)) == 0 {
 			//response.BadRequest(c, "邀请码错误")
-			response.Resp(c, http.StatusBadRequest, "邀请码错误", map[string]interface{}{"id": user.Id})
+			response.Resp(c, http.StatusBadRequest, "邀请码错误", map[string]interface{}{"id": str.IntToString(user.Id)})
 			return
 		}
-		if services.NewInviteCodeService().Create(&models.InviteCode{
+		if err := services.NewInviteCodeService().Create(&models.InviteCode{
 			Code:   req.InviteCode,
 			UserId: user.Id,
-		}) != err {
+		}); err != nil {
 			log.Println("创建邀请码记录失败：", err)
 			//response.BadRequest(c, "邀请码错误")
 			response.Resp(c, http.StatusBadRequest, "邀请码错误", map[string]interface{}{"id": str.IntToString(user.Id)})
