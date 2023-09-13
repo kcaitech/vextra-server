2286c083507a1a40007566f2e37041b0990e8b5b
陈柱锋 <2414802324@qq.com>
2023-09-13 18:07:13 +0800

    集群初始化脚本

--- deployment/k8s-init/mysql/apply.sh
+++ deployment/k8s-init/mysql/apply.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 初始化k8s资源
-kubectl apply -f mysql-configmap.yaml
+kubectl apply -f mysql-cm.yaml
 kubectl apply -f mysql-secert.yaml
 kubectl apply -f mysql-pv.yaml
 kubectl apply -f mysql-statefulset.yaml
-kubectl apply -f mysql-service.yaml
+kubectl apply -f mysql-svc.yaml
--- deployment/k8s-init/percona-mongodb/percona-mongodb/install.sh
+++ deployment/k8s-init/percona-mongodb/percona-mongodb/install.sh
@@ -4,3 +4,4 @@
 
 # 安装
 helm install percona-mongodb percona/psmdb-db -n psmdb -f values.yaml
+kubectl apply -f mongos-nodeport-svc.yaml
