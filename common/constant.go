936725a1d11f283a97b0ad38c4fdf5169e96063e
陈柱锋 <2414802324@qq.com>
2024-01-19 16:04:52 +0800

    .

--- common/constant.go
+++ common/constant.go
@@ -2,10 +2,14 @@ package common
 
 const (
 	ApiVersionPath = "/api/v1"
+
+	// test环境
 	//StorageHost     = "https://storage.protodesign.cn"
 	//FileStorageHost = "https://storage.protodesign.cn/files"
-	StorageHost         = "https://storage1.protodesign.cn"
-	FileStorageHost     = "https://storage2.protodesign.cn"
+	// 正式环境
+	StorageHost     = "https://storage1.protodesign.cn"
+	FileStorageHost = "https://storage2.protodesign.cn"
+
 	ApiGatewayHost      = "apigateway.kc.svc.cluster.local:10000"
 	AuthServiceHost     = "authservice.kc.svc.cluster.local:10001"
 	UserServiceHost     = "userservice.kc.svc.cluster.local:10002"
