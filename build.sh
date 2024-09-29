6706b73b1901eef4e86763dedc51761d45ce167f
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-09-29 18:18:29 +0800

    doc version

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ version=$(cat package.yaml | grep 'version:' | sed 's/[^0-9.]*//g')
 echo version: $version
 echo '---'
 docker build  -t kcserver:$version  -f Dockerfile .
-docker build  -t kcserver-inner:$version  -f Dockerfile-inner .
