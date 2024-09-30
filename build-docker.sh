e7f0c4fe8b8cb9c908fdc8c7038aafe6c6be947e
ruiqiangzh <ruiqiangzh@gmail.com>
2024-10-01 07:20:14 +0800

    build

--- build-docker.sh
+++ build-docker.sh
@@ -2,4 +2,6 @@
 version=$(cat package.yaml | grep 'version:' | sed 's/[^0-9.]*//g')
 echo version: $version
 echo '---'
+docker pull golang:1.22-alpine
+docker pull alpine:3.17
 docker build  -t kcserver:$version  -f Dockerfile .
