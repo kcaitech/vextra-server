f004f878dd919f62e12673ba5167e4ccb14524dc
zhangruiqiang <zhangruiqiang@kcaitech.com>
2024-11-12 09:03:41 +0800

    log panic

--- common/gin/middlewares/error_handler.go
+++ common/gin/middlewares/error_handler.go
@@ -3,8 +3,7 @@ package middlewares
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"runtime"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 	"kcaitech.com/kcserver/common/gin/response"
@@ -15,16 +14,21 @@ func ErrorHandler() gin.HandlerFunc {
 		defer func() {
 			if err := recover(); err != nil {
 				// 获取调用者的信息
-				pc, file, line, ok := runtime.Caller(2)
-				if !ok {
-					file = "???"
-					line = 0
-				}
-				// 获取函数名
-				funcName := runtime.FuncForPC(pc).Name()
+				// pc, file, line, ok := runtime.Caller(2)
+				// if !ok {
+				// 	file = "???"
+				// 	line = 0
+				// }
+				// // 获取函数名
+				// funcName := runtime.FuncForPC(pc).Name()
+				// // 记录错误信息
+				// fmt.Fprintf(os.Stderr, "panic occurred: %v at %s:%d in %s\n", err, file, line, funcName)
 
-				// 记录错误信息
-				fmt.Fprintf(os.Stderr, "panic occurred: %v at %s:%d in %s\n", err, file, line, funcName)
+				// 获取堆栈跟踪信息
+				stackTrace := debug.Stack()
+
+				// 记录错误信息和堆栈跟踪
+				fmt.Fprintf(c.Writer, "panic occurred: %v\nStack Trace:\n%s\n", err, stackTrace)
 
 				// 你可以在这里添加更详细的日志记录，比如使用 zap 或者其他日志库
 				// 例如：logger.Error("panic occurred", zap.Any("error", err), zap.String("file", file), zap.Int("line", line))
