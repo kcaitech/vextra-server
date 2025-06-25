5356de1e82da180f76f0c518e5da8e2aab659092
ruiqiangzh <ruiqiangzh@gmail.com>
2025-06-25 22:31:37 +0800

    config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -33,7 +31,7 @@ type BaseConfiguration struct {
 	StorageUrl struct {
 		Document string `yaml:"document" json:"document"`
 		Attatch  string `yaml:"attatch" json:"attatch"`
-	} `yaml:"storage_url" json:"storage_url"`
+	} `yaml:"storage_public_url" json:"storage_public_url"`
 }
 
 // var Config *BaseConfiguration = &BaseConfiguration{}
@@ -57,61 +55,19 @@ func loadYamlConfig(filePath string, config any) error {
 	}
 	return nil
 }
-func loadJsonConfig(filePath string, config any) error {
-
-	content, err := os.ReadFile(filePath)
-	if err != nil {
-		log.Printf("读取配置文件失败: %v", err)
-		return err
-	}
-
-	err = json.Unmarshal(content, config)
-	if err != nil {
-		log.Printf("配置文件解析失败: %v", err)
-		return err
-	}
-	return nil
-}
-func loadJsonContent(content string, config any) error {
-
-	// content, err := os.ReadFile(filePath)
-	// if err != nil {
-	// 	log.Fatalf("读取配置文件失败: %v", err)
-	// }
-
-	err := json.Unmarshal([]byte(content), config)
-	if err != nil {
-		log.Printf("配置文件解析失败: %v", err)
-		return err
-	}
-	return nil
-}
 
 type Configuration struct {
 	BaseConfiguration `yaml:",inline" json:",inline"`
-	// Wx                struct {
-	// 	Appid  string `yaml:"appid" json:"appid"`
-	// 	Secret string `yaml:"secret" json:"secret"`
-	// } `yaml:"wx" json:"wx"`
-	// WxMp struct {
-	// 	Appid  string `yaml:"appid" json:"appid"`
-	// 	Secret string `yaml:"secret" json:"secret"`
-	// } `yaml:"wxMp" json:"wxMp"`
-	VersionServer struct {
+	VersionServer     struct {
 		Url               string `yaml:"url" json:"url"`
 		MinUpdateInterval int    `yaml:"min_update_interval" json:"min_update_interval"`
 		MinCmdCount       int    `yaml:"min_cmd_count" json:"min_cmd_count"`
-	} `yaml:"version_server" json:"version_server"`
-	Svg2Png struct {
-		Url string `yaml:"url" json:"url"`
-	} `yaml:"svg2png" json:"svg2png"`
+	} `yaml:"doc_update_server" json:"doc_update_server"`
 
-	// Jwt       jwt.JwtConf               `yaml:"jwt" json:"jwt"`
 	Mongo     mongo.MongoConf           `yaml:"mongo" json:"mongo"`
 	Redis     redis.RedisConf           `yaml:"redis" json:"redis"`
 	SafeReiew safereview.SafeReviewConf `yaml:"safe_review" json:"safe_review"`
-	// Snowflake snowflake.SnowflakeConf   `yaml:"snowflake" json:"snowflake"`
-	Storage storage.StorageConf `yaml:"storage" json:"storage"`
+	Storage   storage.Config            `yaml:"storage" json:"storage"`
 
 	DefaultRoute bool `yaml:"default_route,omitempty" json:"default_route,omitempty"`
 	DetailedLog  bool `yaml:"detailed_log,omitempty" json:"detailed_log,omitempty"`
@@ -127,20 +83,4 @@ func LoadYamlFile(filePath string) (*Configuration, error) {
 	var Config Configuration
 	err := loadYamlConfig(filePath, &Config)
 	return &Config, err
-}
-
-func LoadJsonFile(filePath string) (*Configuration, error) {
-	var Config Configuration
-	err := loadJsonConfig(filePath, &Config)
-	return &Config, err
-}
-
-func LoadJsonEnv(env string) (*Configuration, error) {
-	var Config Configuration
-	content := os.Getenv(env)
-	if content == "" {
-		return &Config, errors.New("no " + env)
-	}
-	loadJsonContent(content, &Config)
-	return &Config, nil
 }
