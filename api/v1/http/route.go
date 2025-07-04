7fe0ca0542703ed7b485647c65efd9ef0333b989
张瑞强 <zhangruiqiang@kcaitech.com>
2025-07-04 10:06:01 +0800

    正确处理静态文件

--- api/v1/http/route.go
+++ api/v1/http/route.go
@@ -3,6 +3,8 @@ package http
 import (
 	"context"
 	"net/http"
+	"path/filepath"
+	"slices"
 	"strings"
 
 	"github.com/gin-contrib/static"
@@ -12,6 +14,17 @@ import (
 	"kcaitech.com/kcserver/middlewares"
 	"kcaitech.com/kcserver/services"
 )
+
+var StaticFileSuffix = []string{".html", ".js", ".css", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico", ".woff", ".woff2", ".ttf"}
+
+func isStaticFile(path string) bool {
+	// 先获取path后缀
+	suffix := filepath.Ext(path)
+	if suffix == "" {
+		return false
+	}
+	return slices.Contains(StaticFileSuffix, suffix)
+}
 
 func LoadRoutes(router *gin.Engine) {
 	router.RedirectTrailingSlash = false
@@ -25,10 +38,19 @@ func LoadRoutes(router *gin.Engine) {
 	config := services.GetConfig()
 	// if config.DefaultRoute {
 	router.NoRoute(func(c *gin.Context) {
-		if c.Request.URL.Path == "/api" || strings.HasPrefix(c.Request.URL.Path, "/api/") {
+		path := c.Request.URL.Path
+		if path == "/api" || strings.HasPrefix(path, "/api/") {
 			c.JSON(http.StatusNotFound, gin.H{"error": "auth endpoint not found"})
 			return
 		}
+
+		// 检查是否是对静态文件的请求（HTML、JS、CSS等）
+		if isStaticFile(path) {
+			// 直接尝试提供文件，如果文件不存在则返回404
+			c.File("/app/html" + path)
+			return
+		}
+
 		// 设置缓存时间为15分钟
 		c.Header("Cache-Control", "public, max-age=900")
 		c.File("/app/html/index.html")
