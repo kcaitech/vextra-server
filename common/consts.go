8a6a3a76e1968a23b877529e9d759c26bf362347
张瑞强 <zhangruiqiang@kcaitech.com>
2025-07-09 18:03:01 +0800

    更新redis key

--- common/consts.go
+++ common/consts.go
@@ -1,14 +1,14 @@
 package common
 
 const (
-	RedisKeyDocumentVersioningLastUpdateTime = "server_version_last_update:"
-	RedisKeyDocumentVersioningMutex          = "server_version_mutex:"
-	RedisKeyDocumentVersion                  = "server_version:"
-	RedisKeyDocumentLastCmdVerId             = "server_last_cmd_ver_id:"
-	RedisKeyDocumentComment                  = "server_comment:"
-	RedisKeyDocumentOpMutex                  = "server_op_mutex:"
-	RedisKeyDocumentOp                       = "server_op:"
-	RedisKeyDocumentSelection                = "server_selection:"
-	RedisKeyDocumentSelectionData            = "server_selection_data:"
+	RedisKeyDocumentVersioningLastUpdateTime = "server_document_version_last_update:"
+	RedisKeyDocumentVersioningMutex          = "server_document_version_mutex:"
+	RedisKeyDocumentVersion                  = "server_document_version:"
+	RedisKeyDocumentLastCmdVerId             = "server_document_last_cmd_ver_id:"
+	RedisKeyDocumentComment                  = "server_document_comment:"
+	RedisKeyDocumentOpMutex                  = "server_document_op_mutex:"
+	RedisKeyDocumentOp                       = "server_document_op:"
+	RedisKeyDocumentSelection                = "server_document_selection:"
+	RedisKeyDocumentSelectionData            = "server_document_selection_data:"
 	RedisKeyRateLimit                        = "server_ratelimit:"
 )
