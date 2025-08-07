e4b329f2f4ab9f26f8ee2169fd4af1706ac5f994
张瑞强 <zhangruiqiang@kcaitech.com>
2025-08-07 14:49:24 +0800

    license

--- client/request/document.ts
+++ client/request/document.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023-2025 KCai Technology (https://kcaitech.com). All rights reserved.
+ *
+ * This file is part of the Vextra project, which is licensed under the AGPL-3.0 license.
+ * The full license text can be found in the LICENSE file in the root directory of this source tree.
+ *
+ * For more information about the AGPL-3.0 license, please visit:
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
 import { HttpArgs, HttpMgr } from "./http"
 import { BaseResponse, BaseResponseSchema, PermType, AccessKeyResponseSchema, AccessKeyResponse, ThumbnailResponseDataSchema, ThumbnailResponse, ThumbnailResponseSchema } from "./types"
 import { DocumentInfoSchemaEx, UserInfoSchema, TeamInfoSchema, ProjectInfoSchema, DocumentInfoSchema,  } from "../common/types"
--- client/ws/types.ts
+++ client/ws/types.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023-2025 KCai Technology (https://kcaitech.com). All rights reserved.
+ *
+ * This file is part of the Vextra project, which is licensed under the AGPL-3.0 license.
+ * The full license text can be found in the LICENSE file in the root directory of this source tree.
+ *
+ * For more information about the AGPL-3.0 license, please visit:
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
 import { z } from "zod";
 import { CommentItem } from "../request/comment";
 import { AccessKeyInfoSchema, DocumentInfoSchemaEx, UserInfo } from "../common/types";
